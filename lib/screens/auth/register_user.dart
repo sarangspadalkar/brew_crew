a7826d353e04165bd9f3311f350af0dc58077d94
Sarang Padalkar <padalkar.sarang@gmail.com>
2025-05-11 13:27:17 -0500

    add loading widget

--- lib/screens/auth/register_user.dart
+++ lib/screens/auth/register_user.dart
@@ -1,6 +1,7 @@
 import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
 
@@ -17,91 +18,111 @@ class _RegisterUserState extends State<RegisterUser> {
   final AuthService _auth = AuthService();
   final _registerFormKey = GlobalKey<FormState>();
 
+  bool loading = false;
+
   String email = "";
   String password = "";
   String error = "";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Sign Up to Brew Crew"),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: Icon(Icons.person),
-            onPressed: () {
-              widget.toggleView();
-            },
-            label: Text("Sign In"),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _registerFormKey,
-          child: Column(
-            children: <Widget>[
-              /**Email */
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration('email'),
-                validator:
-                    (value) =>
-                        value == null || !EmailValidator.validate(value)
-                            ? "Enter valid email"
-                            : null,
-                onChanged: (value) {
-                  setState(() => email = value);
-                  print("email ${email}");
+    return loading
+        ? Loading()
+        : Scaffold(
+          backgroundColor: Colors.brown[100],
+          appBar: AppBar(
+            backgroundColor: Colors.brown[400],
+            elevation: 0.0,
+            title: Text("Sign Up to Brew Crew"),
+            actions: <Widget>[
+              TextButton.icon(
+                icon: Icon(Icons.person),
+                onPressed: () {
+                  widget.toggleView();
                 },
+                label: Text("Sign In"),
               ),
-
-              /**Password */
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration('password'),
-                validator:
-                    (value) =>
-                        value == null || value.length < 6
-                            ? "Enter Password 6+ chars long"
-                            : null,
-                onChanged: (value) {
-                  setState(() => password = value);
-                  print("password ${password}");
-                },
-                obscureText: true,
-              ),
-
-              /**Submit Button */
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () async {
-                  bool? validation = _registerFormKey.currentState?.validate();
-                  if (validation != true) {
-                    return;
-                  }
-
-                  User? user = await _auth.registerUser(email, password);
-
-                  if (user == null) {
-                    setState(() => error = "please supply valid user");
-                  }
-                },
-                child: Text("Register", style: TextStyle(color: Colors.white)),
-              ),
-              SizedBox(height: 12.0),
-              Text(error, style: TextStyle(color: Colors.red, fontSize: 14)),
             ],
           ),
-        ),
-      ),
-    );
+          body: Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Form(
+              key: _registerFormKey,
+              child: Column(
+                children: <Widget>[
+                  /**Email */
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration('email'),
+                    validator:
+                        (value) =>
+                            value == null || !EmailValidator.validate(value)
+                                ? "Enter valid email"
+                                : null,
+                    onChanged: (value) {
+                      setState(() => email = value);
+                      print("email ${email}");
+                    },
+                  ),
+
+                  /**Password */
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration('password'),
+                    validator:
+                        (value) =>
+                            value == null || value.length < 6
+                                ? "Enter Password 6+ chars long"
+                                : null,
+                    onChanged: (value) {
+                      setState(() => password = value);
+                      print("password ${password}");
+                    },
+                    obscureText: true,
+                  ),
+
+                  /**Submit Button */
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                        Colors.blue,
+                      ),
+                    ),
+                    onPressed: () async {
+                      bool? validation =
+                          _registerFormKey.currentState?.validate();
+                      if (validation != true) {
+                        return;
+                      }
+
+                      setState(() {
+                        loading = true;
+                      });
+
+                      User? user = await _auth.registerUser(email, password);
+
+                      if (user == null) {
+                        setState(() {
+                          error = "please supply valid user";
+                          loading = false;
+                        });
+                      }
+                    },
+                    child: Text(
+                      "Register",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
   }
 }
--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
@@ -17,89 +18,110 @@ class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _signInFormKey = GlobalKey<FormState>();
 
+  bool loading = false;
+
   String email = "";
   String password = "";
   String error = "";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Sign in to Brew Crew"),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: Icon(Icons.person),
-            onPressed: () {
-              widget.toggleView();
-            },
-            label: Text("Register"),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _signInFormKey,
-          child: Column(
-            children: <Widget>[
-              /**Email */
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration('email'),
-                validator:
-                    (value) =>
-                        value == null || !EmailValidator.validate(value)
-                            ? "Enter valid email"
-                            : null,
-                onChanged: (value) {
-                  setState(() => email = value);
-                  print("email ${email}");
+    return loading
+        ? Loading()
+        : Scaffold(
+          backgroundColor: Colors.brown[100],
+          appBar: AppBar(
+            backgroundColor: Colors.brown[400],
+            elevation: 0.0,
+            title: Text("Sign in to Brew Crew"),
+            actions: <Widget>[
+              TextButton.icon(
+                icon: Icon(Icons.person),
+                onPressed: () {
+                  widget.toggleView();
                 },
+                label: Text("Register"),
               ),
-
-              /**Password */
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration('password'),
-                validator:
-                    (value) =>
-                        value == null || value == "" ? "Password empty" : null,
-                onChanged: (value) {
-                  setState(() => password = value);
-                  print("password ${password}");
-                },
-                obscureText: true,
-              ),
-
-              /**Submit Button */
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () async {
-                  bool? validation = _signInFormKey.currentState?.validate();
-                  if (validation != true) {
-                    return;
-                  }
-
-                  User? user = await _auth.signInUser(email, password);
-
-                  if (user == null) {
-                    setState(() => error = "Error during Sign In");
-                  }
-                },
-                child: Text("Sign In", style: TextStyle(color: Colors.white)),
-              ),
-              SizedBox(height: 12.0),
-              Text(error, style: TextStyle(color: Colors.red, fontSize: 14)),
             ],
           ),
-        ),
-      ),
-    );
+          body: Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Form(
+              key: _signInFormKey,
+              child: Column(
+                children: <Widget>[
+                  /**Email */
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration('email'),
+                    validator:
+                        (value) =>
+                            value == null || !EmailValidator.validate(value)
+                                ? "Enter valid email"
+                                : null,
+                    onChanged: (value) {
+                      setState(() => email = value);
+                      print("email ${email}");
+                    },
+                  ),
+
+                  /**Password */
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration('password'),
+                    validator:
+                        (value) =>
+                            value == null || value == ""
+                                ? "Password empty"
+                                : null,
+                    onChanged: (value) {
+                      setState(() => password = value);
+                      print("password ${password}");
+                    },
+                    obscureText: true,
+                  ),
+
+                  /**Submit Button */
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                        Colors.blue,
+                      ),
+                    ),
+                    onPressed: () async {
+                      bool? validation =
+                          _signInFormKey.currentState?.validate();
+                      if (validation != true) {
+                        return;
+                      }
+
+                      setState(() {
+                        loading = true;
+                      });
+                      User? user = await _auth.signInUser(email, password);
+
+                      if (user == null) {
+                        setState(() {
+                          error = "Error during Sign In";
+                          loading = false;
+                        });
+                      }
+                    },
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
   }
 }
